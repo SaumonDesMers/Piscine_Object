a98aa1783113124734b736b69bd6ac5e8260b67f
Simon Gaubert <sim.gaubert.sg@gmail.com>
2023-10-20 17:44:58 +0200

    feat (day01/ex00): Position, Statistic, Worker

---
+++ day01/ex00/statistic.hpp
@@ -0,0 +1,10 @@
+#ifndef STATS_HPP
+#define STATS_HPP
+
+struct Statistic
+{
+	int level;
+	int exp;
+};
+
+#endif // STATS_HPP
---
+++ day01/ex00/worker.hpp
@@ -0,0 +1,13 @@
+#ifndef WORKER_HPP
+#define WORKER_HPP
+
+#include "position.hpp"
+#include "statistic.hpp"
+
+struct Worker
+{
+	Position position;
+	Statistic statistic;
+};
+
+#endif // WORKER_HPP
